1ec388b094f5ff2eef545ece91c55f160ffad205
ParthaKunduGitHub <mr.pkundu@gmail.com>
2024-11-19 18:06:35 -0800

    Adding initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ucla-ece-m214a-project-sri",
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "jupyter",
+        "notebook"
+    ]
+)
